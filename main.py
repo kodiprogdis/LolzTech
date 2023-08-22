68d91ce3c4fcc715cabbb78112d2a620a9dec2de
kodiprogdis <aidarxabib33487@gmail.com>
2023-08-22 22:00:13 +0300

    Edit main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from commands import Lobby
 
 # Главная функция, которая запускает все
 def start():
-
 	sql_func = Sql_func()
 	sql_func.create_dbx()
 	print(colorama.Fore.RED + "Добро пожаловать в систему управления базой данных игр! Ниже команды:")
